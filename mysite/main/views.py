ea3c46c1ba5d8784c2e9399e808480d662ce3f65
Vasu Garg <vasugarg039@gmail.com>
2026-07-06 14:00:40 +0530

    Rendering Dynamic Content in HTML Templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    context = {'name': 'Django Developer'}
+    return render(request, 'home.html', context)
